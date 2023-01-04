55fb6e95d8b4e60aabf22cae41a690605094bb54
Shvarag <shivam.sc39@gmail.com>
2023-01-05 00:27:18 +0530

    added palindrome and prime with error

---
+++ palindrome.js
@@ -0,0 +1,5 @@
+let s="racecar";
+let b="";
+for(let i=1;i<s.length;i++){
+	b+=s[i]+" ";}
+(b==s)?console.log("No"):console.log("Yes")
---
+++ prime.js
@@ -0,0 +1,9 @@
+let n=13;
+let c=0;
+for(let i=1;i<n;i++){
+	if(n%i==0){
+		c++;}}
+if(c==1){
+	console.log("yes it is prime no")
+}else{
+	console.log("no it is not a prime no")}
