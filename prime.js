bccffaedba16c77ba7a06f083e741a9a540d2492
Deependra yadav <121309012+DeependrYadav@users.noreply.github.com>
2023-01-05 00:46:47 +0530

    code_fixed

--- prime.js
+++ prime.js
@@ -1,9 +1,10 @@
-let n=13;
+let n=23;
 let c=0;
-for(let i=1;i<n;i++){
+for(let i=2;i<n;i++){
 	if(n%i==0){
 		c++;}}
-if(c==1){
+if(c==0){
 	console.log("yes it is prime no")
 }else{
 	console.log("no it is not a prime no")}
+//code_fixed
